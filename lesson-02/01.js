5187f84e6d528894062303b55b4bf2d9abd3d91f
yanakorsak <kazantip2310@gmail.com>
2025-01-15 02:17:20 +0100

    update lesson02-01.js

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,6 +13,10 @@ const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess
+
 
 // your code
+let isAccess;
+isAccess =( isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)
+
+console.log(isAccess)
